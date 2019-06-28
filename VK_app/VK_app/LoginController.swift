20e53a9ca87f55cad11b85b5b03e2ddb418ca0bf
Aleksandr Fetisov <grashey@mail.ru>
2019-06-28 20:38:48 +0300

    закинул основные элементы на экран, добавил фон

---
+++ VK_app/VK_app/LoginController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  VK_app
+//
+//  Created by Aleksandr Fetisov on 28/06/2019.
+//  Copyright © 2019 Aleksandr Fetisov. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+
+}
+
