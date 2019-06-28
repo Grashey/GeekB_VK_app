53d1a8585e1672cb75096a2795472cc41b81b9f1
Aleksandr Fetisov <grashey@mail.ru>
2019-06-28 19:38:40 +0300

    new project  - VK app

---
+++ VK_app/VK_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK_app
+//
+//  Created by Aleksandr Fetisov on 28/06/2019.
+//  Copyright © 2019 Aleksandr Fetisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
