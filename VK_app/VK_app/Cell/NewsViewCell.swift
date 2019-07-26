86216235a74e352a847005688e81da099c72754d
Aleksandr Fetisov <grashey@mail.ru>
2019-07-26 21:13:39 +0300

    добавил иконки

--- VK_app/VK_app/Cell/NewsViewCell.swift
+++ VK_app/VK_app/Cell/NewsViewCell.swift
@@ -12,16 +12,22 @@ class NewsViewCell: UITableViewCell {
     
     @IBOutlet weak var groupAvatar: UIImageView!
     @IBOutlet weak var groupLabel: UILabel!
-    @IBOutlet weak var newsText: UITextView!
-    @IBOutlet weak var newsImage: UIImageView!
-    @IBOutlet weak var likeImage: UIImageView!
-    @IBOutlet weak var likeCount: UILabel!
-    @IBOutlet weak var commentImage: UIImageView!
-    @IBOutlet weak var commentCount: UILabel!
-    @IBOutlet weak var repostImage: UIImageView!
-    @IBOutlet weak var repostCount: UILabel!
-    @IBOutlet weak var viewCountImage: UIImageView!
-    @IBOutlet weak var viewCount: UILabel!
+    @IBOutlet weak var newsTextView: UITextView!
+    @IBOutlet weak var newsImageView: UIImageView!
+    
+    
+    @IBOutlet weak var likeImageView: LikeImageView!
+    @IBOutlet weak var likeCountLabel: UILabel!
+    
+    @IBOutlet weak var commentImageView: UIImageView!
+    @IBOutlet weak var commentCountLabel: UILabel!
+    
+    @IBOutlet weak var repostImageView: UIImageView!
+    @IBOutlet weak var repostCountLabel: UILabel!
+    
+    @IBOutlet weak var viewCountImageView: UIImageView!
+    @IBOutlet weak var viewCountLabel: UILabel!
+
 
     override func awakeFromNib() {
         super.awakeFromNib()
--- VK_app/VK_app/Controllers/NewsViewController.swift
+++ VK_app/VK_app/Controllers/NewsViewController.swift
@@ -11,22 +11,27 @@ import UIKit
 class NewsViewController: UITableViewController {
     
     @IBOutlet var newsTable: UITableView!
-    
+
     var groupAvatar = UIImage()
     var groupName = String()
+    var likeCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //likeCountView.text = String(likeCount)
+        
+        //let likeGR = UITapGestureRecognizer(target: self, action: #selector(heartStateChanged))
+        //likeGR.numberOfTapsRequired = 2
+        //likeImageView.addGestureRecognizer(likeGR)
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
-
+    // MARK: - Tableview methods
+    override func  tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -44,51 +49,14 @@ class NewsViewController: UITableViewController {
 
         return cell
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+   /* @objc private func heartStateChanged(){
+        likeImageView.isHeartFilled.toggle()
+        if likeCountView.text == String(likeCount){
+            likeCountView.text = String(likeCount + 1)
+        } else {
+            likeCountView.text = String(likeCount)
+        }
+    }*/
 
 }
