4c97686d93d6ca98cb6025d16eca8de27a09acdf
Aleksandr Fetisov <grashey@mail.ru>
2019-07-29 21:13:49 +0300

    создал класс для мигающих кнопок (типа loading)

---
+++ VK_app/VK_app/View/LoadingDotView.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingDotView.swift
+//  VK_app
+//
+//  Created by Aleksandr Fetisov on 29/07/2019.
+//  Copyright © 2019 Aleksandr Fetisov. All rights reserved.
+//
+
+import UIKit
+
+class LoadingDotView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
