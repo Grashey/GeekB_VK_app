9928aa82e962c04f6ea476571b1c239f9d933cae
Aleksandr Fetisov <grashey@mail.ru>
2019-07-31 00:11:18 +0300

    добавил анимацию на коллекцию фотографий

--- VK_app/VK_app/Cell/FriendPhotoCell.swift
+++ VK_app/VK_app/Cell/FriendPhotoCell.swift
@@ -13,5 +13,22 @@ class FriendPhotoCell: UICollectionViewCell {
     
     @IBOutlet weak var photoView: UIImageView!
     
+    
+    override func awakeFromNib() {
+        emersion()
+    }
+    
+    @objc func emersion() {
+        self.photoView.transform = CGAffineTransform.identity.scaledBy(x: 0, y: 0)
+        self.photoView.layer.opacity = 0
+        
+        UIView.animate(withDuration: 3, delay: 0, options: UIView.AnimationOptions.curveEaseInOut, animations: {
+            self.photoView.transform = CGAffineTransform.identity
+        }, completion: nil)
+        
+        UIView.animateKeyframes(withDuration: 3, delay: 0, animations: {
+            self.photoView.layer.opacity = 1
+            }, completion: nil)
+        }
 }
 
