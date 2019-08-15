6dd7a0e7c4fb6065acb7e447d9080dfd17555762
Aleksandr Fetisov <grashey@mail.ru>
2019-08-15 20:05:11 +0300

    Alamofire

---
+++ VK_app/VK_app/Cell/NewsPhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  NewsPhotoCell.swift
+//  VK_app
+//
+//  Created by Aleksandr Fetisov on 15/08/2019.
+//  Copyright © 2019 Aleksandr Fetisov. All rights reserved.
+//
+
+import UIKit
+
+class NewsPhotoCell: UICollectionViewCell {
+    
+}
