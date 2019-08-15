a8984b0263ba6d626635932dd89429dba24e587e
Aleksandr Fetisov <grashey@mail.ru>
2019-08-15 12:07:08 +0300

    процесс создания коллекции фото в новостях

---
+++ VK_app/VK_app/View/NewsPhotoCollectionView.swift
@@ -0,0 +1,14 @@
+//
+//  NewsPhotoController.swift
+//  VK_app
+//
+//  Created by Aleksandr Fetisov on 14/08/2019.
+//  Copyright © 2019 Aleksandr Fetisov. All rights reserved.
+//
+
+import UIKit
+
+class NewsPhotoCollectionView: UICollectionView {
+
+  
+}
